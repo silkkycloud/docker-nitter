519a9cd7ca5de3521b0ca2ebedc827267ad36e70
Oscar <silkky@silkky.cloud>
2021-12-06 19:21:45 +1100

    Fix sed command in start script

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 hmac=$(openssl rand -base64 32)
-sed -e "s@secretkey@$hmac@g" /nitter/nitter.conf
+sed -i "s@secretkey@$hmac@g" /nitter/nitter.conf
 
 echo "Giving Redis time to start"
 sleep 2s
