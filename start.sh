766c1aee36177ffa0ea12218cbf7004dec973e82
Oscar <silkky@silkky.cloud>
2022-01-25 08:37:25 +1100

    Fix start script
    Signed-off-by: Oscar <silkky@silkky.cloud>

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 if [[ -v $HMAC_KEY ]]; then
-  sed -i "s@secretkey@$HMAC_KEY@g" /nitter/nitter.conf
-else
   echo "No hmac key!"
   exit 1;
+else
+  sed -i "s@secretkey@$HMAC_KEY@g" /nitter/nitter.conf
 fi
 
 sleep 2s
